7fbcba2e45d67f18f3c0170c9dd071063b60f7da
Can Çetin <can.cetin@sahibinden.com>
2020-04-30 18:27:43 +0300

    Sample angularjs app

--- app/scripts/controllers/page1.js
+++ app/scripts/controllers/page1.js
@@ -5,9 +5,10 @@ angular
     .component('page1', {
         // templateUrl: () => window.accountSettingsBaseDir + 'views/page11.html',
         templateUrl: function () {
-            return window.accountSettingsBaseDir + 'views/page1.html'
+            return window.accountSettingsBaseDir + 'views/page1b.html'
         },
         controller: ['$scope', function ($scope) {
-            $scope.title = "######## Page1 Ctrl";
+            $scope.title = "Page1 scope";
+            $scope.head = "AngularJS APP #2";
         }]
     })
--- app/scripts/controllers/page2.js
+++ app/scripts/controllers/page2.js
@@ -3,8 +3,9 @@
 angular
     .module('exampleApp2')
     .component('page2', {
-        templateUrl: () => window.accountSettingsBaseDir + 'views/page2.html',
+        templateUrl: () => window.accountSettingsBaseDir + 'views/page2b.html',
         controller: ['$scope', function ($scope) {
-            $scope.title = "######### Page2 Ctrl";
+            $scope.title = "Page2 scope";
+            $scope.head = "AngularJS APP #2";
         }]
     })
